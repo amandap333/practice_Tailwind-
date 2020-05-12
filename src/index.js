4c2b6c0a35e1fb5c11f36577bb67a456272a207c
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-05-12 10:54:22 -0600

    using components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './tailwind.generated.css'
+import './tailwind.generated.css';
+
+import Table from './components/table'
+import Navbar from './components/navbar'
 
 const App = () => (
-  <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+  <div className="max-w-md mx-auto flex p-6 bg-blue-100 mt-10 rounded-lg shadow-xl">
     <div className="ml-6 pt-1">
-      <h1 className="text-2xl text-blue-700 leading-tight">
+    <Navbar />
+      <h1 className="text-2xl text-red-500 leading-tight">
         Practicing Tailwind
       </h1>
       <p className="text-base text-gray-700 leading-normal">
         Learning create-react-app and yarn
       </p>
+      <Table />
     </div>
   </div>
 );
