483f1ac21b7b9631e88b070b9cc2edef0c85df00
Amanda Proffit <Amandamarie2567@gmail.comm>
2020-05-12 11:10:45 -0600

    using bg

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import Table from './components/table'
 import Navbar from './components/navbar'
 
 const App = () => (
-  <div className="max-w-md mx-auto flex p-6 bg-blue-100 mt-10 rounded-lg shadow-xl">
+<div className="bg-purple-200" >
+  <div className="max-w-lg mx-auto flex p-6 bg-blue-100 mt-10 rounded-lg shadow-xl">
     <div className="ml-6 pt-1">
     <Navbar />
       <h1 className="text-2xl text-red-500 leading-tight">
@@ -17,6 +18,8 @@ const App = () => (
       </p>
       <Table />
     </div>
+    <div class="rounded-full p-2 m-1 bg-purple-600 text-purple-100  w-40 h-40 flex items-center justify-center text-xl font-bold">Amanda</div>
   </div>
+</div>
 );
 ReactDOM.render(<App />, document.querySelector('#root'));
